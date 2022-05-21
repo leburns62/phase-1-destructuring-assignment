4130f586ec791935222858ca2d30322060a897a6
Levi Hutchinson Burns <leburns62@gmail.com>
2022-05-21 00:21:09 -0400

    commit tree 4c77489980d9b2616c291ca1791ee1acc2b76f28

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+console.log(moo, neigh, baa, oink, cluck);
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+console.log(bessie, dolly, babe, little);
+const [blackAndWhite, ,black, pink, ] = farmAnimals.split(' ');
+console.log(blackAndWhite, black, pink);
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+console.log(red, orange, yellow, green, blue, indigo, violet);
+const [r, o, y, g, b, ,v] = colors;
+console.log(r, o, y, g, b, v);
+const [ , , , , ,indg, ] = colors;
+console.log(indg);
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +21,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +37,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+const { nestedJob, nestedPartner } = nestedMuppet
 
 // Strings
 
